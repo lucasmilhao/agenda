589e5519d3dea50548213c61d3266378135319ea
Lucas Milhão <lucasmilhao683@gmail.com>
2026-03-13 01:57:16 -0300

    Modify SQL script to check for existing database

--- src/main/database/App.sql
+++ src/main/database/App.sql
@@ -1,9 +1,10 @@
+create database if not exists agenda;
 use agenda;
-create database agenda;
 create Table contato(
     contato_id BIGINT PRIMARY key not null AUTO_INCREMENT,
     nome varchar(60),
     telefone varchar(12),
     email varchar(60),
     data_nascimento DATE
+
 )
