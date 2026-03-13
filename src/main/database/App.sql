5b726bbe113fb697ba309c97f85878324918df40
Lucas Milhão <lucasmilhao683@gmail.com>
2026-03-13 01:57:58 -0300

    Modify contato table to enforce not null constraints
    Added 'not null' constraints to nome and data_nascimento fields.

--- src/main/database/App.sql
+++ src/main/database/App.sql
@@ -1,10 +1,11 @@
 create database if not exists agenda;
 use agenda;
 create Table contato(
-    contato_id BIGINT PRIMARY key not null AUTO_INCREMENT,
-    nome varchar(60),
-    telefone varchar(12),
+    contato_id BIGINT PRIMARY key not null AUTO_INCREMENT not null,
+    nome varchar(60) not null,
+    telefone varchar(12) not null,
     email varchar(60),
-    data_nascimento DATE
+    data_nascimento DATE not null
 
 )
+
